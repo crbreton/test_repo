602dd116dde4adeb11b77dea1873c94153bd7b65
crbreton <connor.breton@gmail.com>
2017-10-24 20:06:09 -0500

    Add in quadratic

--- makeLine.R
+++ makeLine.R
@@ -3,9 +3,12 @@
 
 x <- -10:10
 b <- -2
+b2 <- 4
 a <- 100
 y <- b*x + a
-
+y2 <- b2*x^2 + b*x + a
 
 plot(x, y, xlab = "Independent variable", ylab = "Dependent variable",
+     type = "l")
+plot(x, y2, xlab = "Independent variable", ylab = "Dependent variable",
      type = "l")
